e6f9bbe11ea8063e7b40a22a5c187a771726fad9
chenxun233 <158007000+chenxun233@users.noreply.github.com>
2026-03-14 21:41:40 +0800

    deleted unnecessary files

--- obj_dir/rc_gearbox_sim.d
+++
@@ -1,6 +0,0 @@
-rc_gearbox_sim.o: ../verilator_src/rc_gearbox_sim.cpp VRC_gearbox256.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h
